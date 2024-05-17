5e4d98d8a29db8d07424549b05be5005a10a0f39
Jorge Andres <opandres123@gmail.com>
2024-05-16 21:27:46 -0500

    Panel Epleado  basico

---
+++ panelEmpleado.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>EMPLEADOS</title>
+    </head>
+<body>
+    <H2>INGRESO EMPLEADOS:</H2>
+    <p>Este es el ingreso para los empleados</p>
+</body>
+</html>
