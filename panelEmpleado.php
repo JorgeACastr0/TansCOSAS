318ae2a18416f27e2ed7e4a57ed572e1e4048558
Brayhan Ortiz <brayhanortiz27@gmail.com>
2024-05-20 00:57:31 -0500

    agregar items

--- panelEmpleado.php
+++ panelEmpleado.php
@@ -1,12 +1,46 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>EMPLEADOS</title>
-        <link rel="stylesheet" href="css/Style.css">
-    </head>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/bcb64c38d6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/Style.css">
+
+</head>
+<header>
+    <nav>
+        <ul>
+            <li><a href="#panelEmpleado.php">INGRESO EMPLEADOS</a></li>
+            <li><a href="#ruta">RUTAS</a></li>
+            <li><a href="#vehiculos">VEHICULOS</a></li>
+            <li><a href="#cargas">CARGAS</a></li>
+
+        </ul>
+    </nav>
+
+</header>
+
 <body>
-    <div class="">
-    <H2>INGRESO EMPLEADOS:</H2>
-    <p>Este es el ingreso para los empleados</p>
+    <div id="panelEmpleado" class="content-section">
+        <!-- Contenido de ingreso de empleados -->
+    </div>
+
+    <div id="ruta" class="content-section">
+        <!-- Contenido de rutas -->
+    </div>
+
+    <div id="vehiculos" class="content-section">
+        <!-- Contenido de vehículos -->
+    </div>
+
+    <div id="cargas" class="content-section">
+        <!-- Contenido de cargas -->
+    </div>
+
+
 </body>
+
 </html>
